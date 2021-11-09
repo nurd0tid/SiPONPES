8124e0ff4368265b4fc4f9e18129613bbb7613aa
nurd0tid <mnur29054@gmail.com>
2021-11-09 17:53:33 +0700

    Proges CRUD Identitas

---
+++ database/migrations/2021_11_09_173704_create_identitas_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\Expr\Cast\String_;
+
+class CreateIdentitasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('identitas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('npsn', 8);
+            $table->string('nss', 12);
+            $table->text('alamat');
+            $table->string('zipcode', 5);
+            $table->string('telp', 13);
+            $table->string('kelurahan');
+            $table->string('kecamatan');
+            $table->string('kota');
+            $table->string('provinsi');
+            $table->string('website');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('identitas');
+    }
+}
